8534a9d8f85fb5f1f9e2ffffa265ef8b1eb8475f
kitty jutbring <kittyj@cmail.nu>
2016-01-05 23:58:23 +0100

    rename

---
+++ torRedirect.js
@@ -0,0 +1,14 @@
+/*
+ * Tor-redirect-express
+ * Redirect tor users to using your .onion site
+ * 
+ * Author: kjutbring
+ * Licence: GPL-3.0
+ *
+ */
+
+module.exports = {
+    redirect: function(req, res, next) {
+        console.log(res);
+    };
+}
